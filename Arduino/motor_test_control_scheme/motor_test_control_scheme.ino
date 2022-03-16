fb2e0326ef2673b45479d536d6439f02f60b0703
Urban Pistek <urban.pistek@gmail.com>
2022-03-16 15:58:07 -0400

    Saving progress

--- Arduino/motor_test_control_scheme/motor_test_control_scheme.ino
+++ Arduino/motor_test_control_scheme/motor_test_control_scheme.ino
@@ -5,11 +5,16 @@ const int ML_PWM_PIN = 11;
 const int ML_DIR_PIN = 8;
 const int MR_PWM_PIN = 10;
 const int MR_DIR_PIN = 7;
+const int R_ENCODER_PIN = 13;
+const int L_ENCODER_PIN = 6;
 
 int set = 0;
 
+unsigned long lastTime = millis();
+float *encoderValues;
+
 // Declare drive object
-Drive drive(ML_PWM_PIN, MR_PWM_PIN, ML_DIR_PIN, MR_DIR_PIN);
+Drive drive(ML_PWM_PIN, MR_PWM_PIN, ML_DIR_PIN, MR_DIR_PIN, L_ENCODER_PIN, R_ENCODER_PIN);
 
 void setup() {
   Serial.begin(9600);
@@ -35,58 +40,70 @@ void loop() {
   }
 
   Serial.println("==> Running Main()");
-
-  // Testing accelerating 
-  drive.accelerate(ACC_MINIMAL, true);
-  drive.get_params(&curr_speed, &curr_rpm, &mode);
-  Serial.println(curr_speed);
-  Serial.println(curr_rpm);
-  Serial.println(mode);
-
-  delay(5000);
-  
-  // Test decceleration
-  drive.deccelerate(ACC_MINIMAL, true);
-  drive.get_params(&curr_speed, &curr_rpm, &mode);
-  Serial.println(curr_speed);
-  Serial.println(curr_rpm);
-  Serial.println(mode);
-
-  delay(5000);
-
-  // Turn right
-  drive.turn_right(HALF_SPEED);
-  drive.get_params(&curr_speed, &curr_rpm, &mode);
-  Serial.println(curr_speed);
-  Serial.println(curr_rpm);
-  Serial.println(mode);
-
-  delay(5000);
-
-  // Turn left
-  drive.turn_left(HALF_SPEED);
-  drive.get_params(&curr_speed, &curr_rpm, &mode);
-  Serial.println(curr_speed);
-  Serial.println(curr_rpm);
-  Serial.println(mode);
-
-  delay(5000);
+//
+//  // Testing accelerating 
+//  drive.accelerate(ACC_MINIMAL, true);
+//  drive.get_params(&curr_speed, &curr_rpm, &mode);
+//  Serial.println(curr_speed);
+//  Serial.println(curr_rpm);
+//  Serial.println(mode);
+//
+//  delay(5000);
+//  
+//  // Test decceleration
+//  drive.deccelerate(ACC_MINIMAL, true);
+//  drive.get_params(&curr_speed, &curr_rpm, &mode);
+//  Serial.println(curr_speed);
+//  Serial.println(curr_rpm);
+//  Serial.println(mode);
+//
+//  delay(5000);
+//
+//  // Turn right
+//  drive.turn_right(HALF_SPEED);
+//  drive.get_params(&curr_speed, &curr_rpm, &mode);
+//  Serial.println(curr_speed);
+//  Serial.println(curr_rpm);
+//  Serial.println(mode);
+//
+//  delay(5000);
+//
+//  // Turn left
+//  drive.turn_left(HALF_SPEED);
+//  drive.get_params(&curr_speed, &curr_rpm, &mode);
+//  Serial.println(curr_speed);
+//  Serial.println(curr_rpm);
+//  Serial.println(mode);
+//
+//  delay(5000);
 
   // Cruise at a constant speed
-  drive.cruise(HALF_SPEED, true);
+  drive.cruise(MAX_SPEED, true);
   drive.get_params(&curr_speed, &curr_rpm, &mode);
   Serial.println(curr_speed);
   Serial.println(curr_rpm);
   Serial.println(mode);
 
-  delay(5000);
+  delay(500);
+//
+//  // E-stop
+//  drive.estop();
+//  drive.get_params(&curr_speed, &curr_rpm, &mode);
+//  Serial.println(curr_speed);
+//  Serial.println(curr_rpm);
+//  Serial.println(mode);
+//
+//  delay(5000); 
 
-  // E-stop
-  drive.estop();
-  drive.get_params(&curr_speed, &curr_rpm, &mode);
-  Serial.println(curr_speed);
-  Serial.println(curr_rpm);
   Serial.println(mode);
-
+  encoderValues = drive.encoderReading(&lastTime, HALF_SPEED);
+  for ( int i = 0; i < 2; i++ ) {
+    Serial.println("Encoder value");
+    Serial.println(*(encoderValues + i));
+   }
+  drive.get_params(&curr_speed, &curr_rpm, &mode);
   delay(5000); 
+  Serial.println(curr_speed);
+  Serial.println(curr_rpm);
+  Serial.println(mode);
 }
