2cc4cf455232c30a3a4f9ec91503ca232ed3552e
ShyamHari <shyamu.hari@gmail.com>
2022-03-19 17:05:28 -0400

    I love Parth and I love his code more

--- Arduino/main_algo/integration_testing/integration_testing.ino
+++ Arduino/main_algo/integration_testing/integration_testing.ino
@@ -18,9 +18,23 @@
 #define ENC_PIN_LEFT D5
 #define ENC_PIN_RIGHT D8
 
+
+Adafruit_ICM20948 icm;
+
+int* echo_array = new int[D1, D2];
+HCSR04 hc(D0, echo_array, NUM_US); 
+Sensors ATAT(2); 
+
+
 Drive motor_control(PWM_PIN_LEFT, PWM_PIN_RIGHT, DIR_PIN_LEFT, DIR_PIN_RIGHT);
+float ultrasonicArrayVals[10] = {0}; 
+float us_array[NUM_US] = {0, 0};
+float imu_array[NUM_SENS] = {0, 0, 0, 0, 0, 0};
+
 void setup() {
-  Serial.begin(9600); 
+//  Wire.begin(); 
+  Serial.begin(115200); 
+  ATAT.ultrasonicSetup(D0, D1, D2);
   // put your setup code here, to run once:
 
 }
@@ -30,26 +44,53 @@ void loop() {
   int curr_rpm = 0;
   int modeMotor = 0;
   // put your main code here, to run repeatedly:
-  motor_control.accelerate(ACC_MINIMAL, true);
-  motor_control.get_params(&curr_speed, &curr_rpm, &modeMotor);
-  Serial.println(curr_speed);
-  Serial.println(curr_rpm);
-  Serial.println(modeMotor);
-  delay(5000);
-  motor_control.estop(); 
-  delay(2000); 
-  motor_control.accelerate(ACC_MINIMAL, false);
-  motor_control.get_params(&curr_speed, &curr_rpm, &modeMotor);
-  Serial.println(curr_speed);
-  Serial.println(curr_rpm);
-  Serial.println(modeMotor);
-  motor_control.estop(); 
-  motor_control.turn_right(MAX_SPEED); 
-  delay(10000); 
-  motor_control.estop(); 
-  motor_control.turn_left(MAX_SPEED); 
-  delay(10000); 
-  motor_control.estop(); 
-  delay(10000);
+  float * ultrasonicReadings = new float[NUM_US]; 
+  float * imuReadings = new float[NUM_SENS];
+//  motor_control.accelerate(ACC_MINIMAL, true);
+//  motor_control.get_params(&curr_speed, &curr_rpm, &modeMotor);
+//  Serial.println(curr_speed);
+//  Serial.println(curr_rpm);
+//  Serial.println(modeMotor);
+//  delay(5000);
+//  motor_control.estop(); 
+//  delay(2000); 
+//  motor_control.accelerate(ACC_MINIMAL, false);
+//  motor_control.get_params(&curr_speed, &curr_rpm, &modeMotor);
+//  Serial.println(curr_speed);
+//  Serial.println(curr_rpm);
+//  Serial.println(modeMotor);
+//  motor_control.estop(); 
+//  motor_control.turn_right(MAX_SPEED); 
+//  delay(1000); 
+//  motor_control.estop(); 
+//  motor_control.turn_left(MAX_SPEED); 
+//  delay(1000); 
+//  motor_control.estop(); 
+//  delay(1000);
+//  ATAT.readUltrasonic(&hc, ultrasonicReadings); 
+
+//  Serial.println("In while loop"); 
+
+  // Parth's US shit
+//  for (int i=0; i<30; i++){
+//    ATAT.readUltrasonicBetter(ultrasonicReadings);
+//    Serial.print("Front: ");
+//    Serial.println(ultrasonicReadings[0]);
+//  
+//    Serial.print("Left: ");
+//    Serial.println(ultrasonicReadings[1]);
+//    delay(100);
+//  }
+  ATAT.readICM(&icm, imuReadings);
+  for(int i = 0; i < NUM_SENS; i++){ 
+    Serial.println(imuReadings[i]);
+  }
+  delay(1000);
+  ultrasonicReadings = NULL;
+  imuReadings = NULL;
+  delete[] imuReadings;
+  delete[] ultrasonicReadings; 
+
+  
 
 }
