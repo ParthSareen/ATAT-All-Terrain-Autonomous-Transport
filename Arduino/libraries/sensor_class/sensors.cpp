f347c18a32f336873856a5801ac5ad5f4d077c94
ShyamHari <shyamu.hari@gmail.com>
2022-03-19 17:05:28 -0400

    I love Parth and I love his code more

--- Arduino/libraries/sensor_class/sensors.cpp
+++ Arduino/libraries/sensor_class/sensors.cpp
@@ -3,14 +3,20 @@
 #include "MPU6050.h"
 #include "Wire.h"
 #include "HCSR04.h"
+#include "Adafruit_ICM20X.h"
+#include "Adafruit_ICM20948.h"
+#include "Adafruit_Sensor.h"
 
 #define OUTPUT_READABLE_ACCELGYRO
-
+#define ICM_CS 10
+#define ICM_SCK 13
+#define ICM_MISO 12
+#define ICM_MOSI 11
 // ============== Public Methods ==============
 
-Sensors::Sensors(int trig_pin, int echo_pin[3], int num_sensors){
-  _trig_pin = trig_pin;
-  _echo_pin = echo_pin;
+Sensors::Sensors(int num_sensors){
+  // _trig_pin = trig_pin;
+  // _echo_pin = echo_pin;
   _num_sensors = num_sensors;
 }
 
@@ -44,6 +50,19 @@ bool Sensors::calibrateUltrasonic(int num_readings, HCSR04 hc){
   Serial.println("Calibration Success");
   return true;
 }
+
+float checkDeadbandValue (float val, float minVal)
+// return value with deadband
+{
+  float curVal = val;
+  if (val < minVal and val > -minVal)
+  {
+    curVal = 0;
+  }
+  return curVal;
+}
+
+
 
 /**
 * Calibrating IMU
@@ -238,14 +257,54 @@ int * Sensors::calibrateIMU(MPU6050 accelgyro){
 * Returns: Integer Array of Ultrasonic Values
 */
 
-void Sensors::readUltrasonic(HCSR04 *hc, float usReadings[3]){
+void Sensors::readUltrasonic(HCSR04 *hc, float usReadings[2]){
   Serial.println("==> Reading Ultrasonic Sensor");
   // static float ultrasonicReadings[3];
   // float* ultrasonicReadings = new float[3];
   for (int i = 0; i < _num_sensors; i++){
-    usReadings[i] = hc->dist(i);
+    float val = hc->dist(i);
+    Serial.print("hello from inside: ");
+    Serial.println(val);
+    usReadings[i] = val;
   }
   return;
+}
+
+void Sensors::ultrasonicSetup(int trig_pin, int echo_pin_front, int echo_pin_left){
+  // TODO: Refactor
+  pinMode(trig_pin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echo_pin_left, INPUT);
+  pinMode(echo_pin_front, INPUT);
+  _trig_pin = trig_pin;
+  _echo_pin_left = echo_pin_left;
+  _echo_pin_front = echo_pin_front;
+}
+
+void Sensors::readUltrasonicBetter(float usReadings[2]){
+  // TODO cleanup
+  // US Front
+  digitalWrite(_trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(_trig_pin, HIGH);
+  delayMicroseconds(10);
+  float duration_front = pulseIn(_echo_pin_front, HIGH);
+  float distance_front = duration_front *0.034/2;
+  // Serial.print("Front: ");
+  // Serial.println(distance_front);
+  usReadings[0] = distance_front;
+
+  delay(20);
+
+  // US Left
+  digitalWrite(_trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(_trig_pin, HIGH);
+  delayMicroseconds(10);
+  float duration_left = pulseIn(_echo_pin_left, HIGH);
+  float distance_left = duration_left*0.034/2;
+  // Serial.print("Left: ");
+  // Serial.println(distance_left);
+  usReadings[1] = distance_left;
 }
 
 /**
@@ -285,4 +344,77 @@ int16_t * Sensors::readIMU(MPU6050 accelgyro){
     Serial.println(gz);
     delay(1000);
     return IMU_Values;
+}
+
+void Sensors::calibrateICM(Adafruit_ICM20948* icm) {
+  while (!Serial)
+     delay(10); // will pause Zero, Leonardo, etc until serial console opens
+  //   Wire.begin(D1,D2);
+  Serial.println("Adafruit ICM20948 test!");
+  // Try to initialize!
+  if (!icm->begin_I2C()) {
+    // if (!icm.begin_SPI(ICM_CS)) {
+    // if (!icm.begin_SPI(ICM_CS, ICM_SCK, ICM_MISO, ICM_MOSI)) {
+    Serial.println("Failed to find ICM20948 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("ICM20948 Found!");
+  // icm.setAccelRange(ICM20948_ACCEL_RANGE_16_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (icm->getAccelRange()) {
+  case ICM20948_ACCEL_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case ICM20948_ACCEL_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case ICM20948_ACCEL_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case ICM20948_ACCEL_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  Serial.println("OK");
+  // icm.setGyroRange(ICM20948_GYRO_RANGE_2000_DPS);
+  Serial.print("Gyro range set to: ");
+  switch (icm->getGyroRange()) {
+  case ICM20948_GYRO_RANGE_250_DPS:
+    Serial.println("250 degrees/s");
+    break;
+  case ICM20948_GYRO_RANGE_500_DPS:
+    Serial.println("500 degrees/s");
+    break;
+  case ICM20948_GYRO_RANGE_1000_DPS:
+    Serial.println("1000 degrees/s");
+    break;
+  case ICM20948_GYRO_RANGE_2000_DPS:
+    Serial.println("2000 degrees/s");
+    break;
+  }
+  //  icm.setAccelRateDivisor(4095);
+  uint16_t accel_divisor = icm->getAccelRateDivisor();
+  float accel_rate = 1125 / (1.0 + accel_divisor);
+  Serial.print("Accelerometer data rate divisor set to: ");
+  Serial.println(accel_divisor);
+  Serial.print("Accelerometer data rate (Hz) is approximately: ");
+  Serial.println(accel_rate);
+}
+
+void Sensors::readICM(Adafruit_ICM20948* icm, float icmReadings[6]){ 
+  Serial.println("==> Reading IMU Sensor");
+  sensors_event_t accel;
+  sensors_event_t gyro;
+  sensors_event_t temp; 
+  sensors_event_t mag; 
+  icm->getEvent(&accel, &gyro, &temp, &mag);
+  icmReadings[0] = accel.acceleration.x; 
+  icmReadings[1] = accel.acceleration.y; 
+  icmReadings[2] = accel.acceleration.z; 
+  icmReadings[3] = gyro.gyro.x;
+  icmReadings[4] = gyro.gyro.y;
+  icmReadings[5] = gyro.gyro.z;
+  return;
 }
