7c7a92fb209db2d882f974b91e108b1aa18528b8
Parth Sareen <psareen@uwaterloo.ca>
2022-03-19 13:51:31 -0400

    Test file for telemetry

--- Arduino/libraries/telemetry/telemetry_test/telemetry_test.ino
+++ Arduino/libraries/telemetry/telemetry_test/telemetry_test.ino
@@ -8,12 +8,19 @@
 #include <Telemetry.h>
 
 char ssid[] = "TP-LINK_2.4GHz_677647";    //  your network SSID (name) 
+//char ssid[] = "TP-LINK_5GHz_677648";    //  your network SSID (name) 
+//char ssid[] = "k";
+
+//char ssid[] = "iPhone";
 char pass[] = "98238316";   // your network password
-const uint16_t port  = 10101;
-const char* addr = "192.168.0.101";
+//char pass[] = "2FEE5A9E144A";
+//char pass[] = "6cqvsnmh779pt";
+uint16_t port  = 10101;
+//char* addr = "172.20.10.7";
+char* addr = "192.168.0.101";
 WiFiClient  client;
 
-Telemetry telemetry(1000);
+Telemetry telemetry(1000, &client, addr, port);
 
 void setup() {
   Serial.begin(115200);
@@ -33,6 +40,8 @@ void setup() {
   Serial.print("WiFi connected, ");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  Serial.println("Running Telemetry Tests");
 }
 
 void loop() {
@@ -46,14 +55,26 @@ void loop() {
 //    return;
 //  }
 
-  // put your main code here, to run repeatedly:
-  bool upload_status = telemetry.uploadUltrasonic(10.2, 22.22, 33.33, client);
-  if (upload_status){
-    Serial.println("US Upload Successful");
-  }
-  else {
-    Serial.println("US Upload Failed");
-  }
+  unsigned long start = micros();
+  Serial.print("Ultrasonic Test Status: ");
+  bool ultrasonic_test = telemetry.uploadUltrasonic(20, 10, 2.3);
+  Serial.println(ultrasonic_test);
+
+  Serial.print("IMU Acceleration Test Status: ");
+  bool imu_test_acc = telemetry.uploadImuAccel(1, 1.1, 12);
+  Serial.println(imu_test_acc);
+
+  Serial.print("IMU Gyro Test Status: ");
+  bool imu_test_gyro = telemetry.uploadImuGyro(0, 2, 4);
+  Serial.println(imu_test_gyro);
+
+  Serial.print("Orientation Test Status: ");
+  bool robot_orientation_test = telemetry.uploadOrientation(3);
+  Serial.println(robot_orientation_test);
+  unsigned long end = micros();
+  unsigned long delta = end - start;
+  Serial.print("Upload of all data took: ");
+  Serial.println(delta);
   delay(5000);
 
 }
