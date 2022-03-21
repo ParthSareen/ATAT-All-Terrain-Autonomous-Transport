a7cae50335af3e99ba8bebf8af3c6f2e314d7288
ParthSareen <psareen@uwaterloo.ca>
2022-03-21 13:59:44 -0400

    Added ultrasonic reading which is reliable

--- Arduino/libraries/sensors/sensors.h
+++ Arduino/libraries/sensors/sensors.h
@@ -8,17 +8,18 @@
 #include "Adafruit_ICM20948.h"
 #include "Adafruit_Sensor.h"
 class Sensors {
-  public:
-    Sensors(int trig_pin, int echo_pin[3], int num_sensors);
-    void readUltrasonic(HCSR04 *hc, float usReadings[3]);
-    int16_t * readIMU(MPU6050 accelgyro);
+public:
+    Sensors(int num_sensors);
     bool calibrateUltrasonic(int num_readings, HCSR04 hc);
-    int * calibrateIMU(MPU6050 accelgyro);
     void calibrateICM(Adafruit_ICM20948* icm);
     void readICM(Adafruit_ICM20948* icm,  float icmReadings[6]);
+    void ultrasonicSetup(int trig_pin, int echo_pin_front, int echo_pin_left);
+    void readUltrasonicBetter(float usReadings[2]);
 
-  private:
+private:
     int _trig_pin;
+    int _echo_pin_front;
+    int _echo_pin_left;
     int _num_sensors;
     int* _echo_pin;
 };
